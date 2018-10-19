745ea433970d3fd582529a2061700f9ebbcc8a78
Klaus-Uwe Mitterer <git@kumi.email>
2018-10-19 18:21:59 +0200

    Improve web interface

---
+++ server/static/worker.js
@@ -0,0 +1,50 @@
+$body = $("body");
+
+function lockform() {
+        $("#theform :input").prop("disabled", true);
+        $body.addClass("loading");
+}
+
+function unlockform() {
+        $("#theform :input").prop("disabled", false);
+        $body.removeClass("loading");
+}
+
+$('#theform').submit(function(event){
+    event.preventDefault();
+    if(this.checkValidity()) { 
+            $.ajax({
+                    type: "POST",
+                    url: "/addjob",
+                    data: $('#theform').serialize(),
+                    success: function(msg){
+                            lockform();
+                            interval = setInterval(checkServerForFile,3000,msg);
+                            function checkServerForFile(jobid) {
+                                    $.ajax({
+                                            type: "GET",
+                                            cache: false,
+                                            url: "/getjob/" + jobid,
+                                            statusCode: {
+                                                    404: function() {
+                                                            clearInterval(interval);
+                                                            unlockform();
+                                                    },
+                                                    200: function() {
+                                                            clearInterval(interval);
+                                                            unlockform();
+                                                            window.location.href = "/getjob/" + jobid
+                                                    },
+                                                    500: function() {
+                                                            clearInterval(interval);
+                                                            window.alert("Failed to process request.")
+                                                            unlockform();
+                                                    }
+                                            }
+                                    });
+                                }
+
+                        }
+                });}
+});
+
