f8eb917a940908af5a0562ea0e6946b0a177a2eb
Klaus-Uwe Mitterer <git@kumi.email>
2018-10-21 11:07:03 +0200

    Prevent multiple downloads in Chrome

--- server/static/worker.js
+++ server/static/worker.js
@@ -7,50 +7,55 @@ function toggleOptions() {
 }
 
 function lockform() {
-        $("#theform :input").prop("disabled", true);
-        $body.addClass("loading");
+	$("#theform :input").prop("disabled", true);
+	$body.addClass("loading");
 }
 
 function unlockform() {
-        $("#theform :input").prop("disabled", false);
-        $body.removeClass("loading");
+	$("#theform :input").prop("disabled", false);
+	$body.removeClass("loading");
 }
 
 $('#theform').submit(function(event){
-    event.preventDefault();
-    if(this.checkValidity()) { 
-            $.ajax({
-                    type: "POST",
-                    url: "/addjob",
-                    data: $('#theform').serialize(),
-                    success: function(msg){
-                            lockform();
-                            interval = setInterval(checkServerForFile,3000,msg);
-                            function checkServerForFile(jobid) {
-                                    $.ajax({
-                                            type: "GET",
-                                            cache: false,
-                                            url: "/getjob/" + jobid,
-                                            statusCode: {
-                                                    404: function() {
-                                                            clearInterval(interval);
-                                                            unlockform();
-                                                    },
-                                                    200: function() {
-                                                            clearInterval(interval);
-                                                            unlockform();
-                                                            window.location.href = "/getjob/" + jobid
-                                                    },
-                                                    500: function() {
-                                                            clearInterval(interval);
-                                                            window.alert("Failed to process request. The URL may be incorrect or unsupported.")
-                                                            unlockform();
-                                                    }
-                                            }
-                                    });
-                                }
+	event.preventDefault();
+	if(this.checkValidity()) { 
+		$.ajax({
+			type: "POST",
+			url: "/addjob",
+			data: $('#theform').serialize(),
+			success: function(msg){
+				lockform();
+				interval = setInterval(checkServerForFile,3000,msg);
+				window.panaxworking = false;
+				function checkServerForFile(jobid) {
+					if (!window.panaxworking) {
+						window.panaxworking = true;
+						$.ajax({
+							type: "GET",
+							cache: false,
+							url: "/getjob/" + jobid,
+							statusCode: {
+								404: function() {
+									clearInterval(interval);
+									unlockform();
+								},
+								200: function() {
+									clearInterval(interval);
+									unlockform();
+									window.location.href = "/getjob/" + jobid
+								},
+								500: function() {
+									clearInterval(interval);
+									window.alert("Failed to process request. The URL may be incorrect or unsupported.")
+									unlockform();
+								}
+							}
+						});
+						window.panaxworking = false;
+					};
+				}
 
-                        }
-                });}
+			}
+		});}
 });
 
