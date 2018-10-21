dc8bb467345742b1a80fb59e9273c985e9110fa8
Klaus-Uwe Mitterer <git@kumi.email>
2018-10-21 19:54:01 +0200

    Show file title in web frontend

--- server/static/worker.js
+++ server/static/worker.js
@@ -23,24 +23,24 @@ function deletecard(jobid) {
 }
 
 
-function addcard(jobid) {
-        var text = '<div class="col-sm-3" id="' + jobid + '"> <div class="card"> <img class="card-img-top img-fluid" src="/spinner.gif" alt="Creating Image"> </div> </div>';
+function addcard(jobid, title) {
+        var text = '<div class="col-sm-3" id="' + jobid + '"> <div class="card"> <img class="card-img-top img-fluid" src="/spinner.gif" alt="Creating Image"><div style="text-align: center; font-weight: bold;" class="card-block">' + title + '</div> </div> </div>';
         $('#cards').append(text);
 	$('html,body').animate({scrollTop: $('#' + jobid).offset().top});
 }
 
-function failcard(jobid) {
-        deletecard(jobid);
+function failcard(jobid, title) {
+        deletecard(jobid, title);
 
-        var text = '<div class="col-sm-3" id="' + jobid + '"> <div class="card"> <div style="text-align: center; color: red; font-weight: bold;" class="card-block">Export failed.</div><div style="text-align: center;" class="card-block"> <a style="color: white;" onclick="deletecard(\'' + jobid + '\');" class="btn btn-danger">Hide</a></div> </div> </div>';
+        var text = '<div class="col-sm-3" id="' + jobid + '"> <div class="card"> <div style="text-align: center; color: red; font-weight: bold;" class="card-block">' + title + ': Export failed.</div><div style="text-align: center;" class="card-block"> <a style="color: white;" onclick="deletecard(\'' + jobid + '\');" class="btn btn-danger">Hide</a></div> </div> </div>';
         $('#cards').append(text);
 }
 
 
-function finishcard(jobid) {
+function finishcard(jobid, title) {
 	deletecard(jobid);
 
-	var text = '<div class="col-sm-3" id="' + jobid + '"> <div class="card"> <img class="card-img-top img-fluid" src="/getjob/' + jobid + '" alt="Final Image"> <div style="text-align: center; color: white;" class="card-block"> <a href="/getjob/' + jobid + '" class="btn btn-primary">Download</a> <a onclick="deletecard(\'' + jobid + '\');" class="btn btn-danger">Hide</a></div> </div> </div>';
+	var text = '<div class="col-sm-3" id="' + jobid + '"> <div class="card"> <img class="card-img-top img-fluid" src="/getjob/' + jobid + '" alt="Final Image"><div style="text-align: center; font-weight: bold;" class="card-block">' + title + '</div> <div style="text-align: center; color: white;" class="card-block"> <a href="/getjob/' + jobid + '" class="btn btn-primary">Download</a> <a onclick="deletecard(\'' + jobid + '\');" class="btn btn-danger">Hide</a></div> </div> </div>';
 	$('#cards').append(text);
 }
 
@@ -52,11 +52,13 @@ $('#theform').submit(function(event){
 			url: "/addjob",
 			data: $('#theform').serialize(),
 			success: function(msg){
-				var interval = setInterval(checkServerForFile,3000,msg);
+				var title = ($("#title").val() ? $("#title").val() : "No title");
+				var interval = setInterval(checkServerForFile,3000,msg, title);
 				window.panaxworking = false;
-				addcard(msg);
+				addcard(msg, title);
 
-				function checkServerForFile(jobid) {
+				function checkServerForFile(jobid, title) {
+
 					if (!window.panaxworking) {
 						window.panaxworking = true;
 						$.ajax({
@@ -66,17 +68,17 @@ $('#theform').submit(function(event){
 							statusCode: {
 								404: function() {
 									clearInterval(interval);
-									failcard(jobid);
+									failcard(jobid, title);
 									return;
 								},
 								200: function() {
 									clearInterval(interval);
-									finishcard(jobid);
+									finishcard(jobid, title);
 									return;
 								},
 								500: function() {
 									clearInterval(interval);
-									failcard(jobid);
+									failcard(jobid, title);
 									return;
 								}
 							}
